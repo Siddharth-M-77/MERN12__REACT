e776f443c709a27faa25e3e9a3ce3725ea065783
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-11-21 20:30:30 +0530

    done-formhandling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LiveClock from './components/LiveClock'
+import FormHandling from './components/FormHandling'
 
 const App = () => {
   return (
     <div className='w-screen h-screen '>
       {/* <LiveClock/> */}
-      
+      <FormHandling/>
+
       
     </div>
   )
